c0d25626c0961623e99664fe95a130de2455e496
ismeet010 <ismeet.k.chhabda@gmail.com>
2025-05-30 11:41:43 +0530

    deploy in github pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
+  base: "/customer-dashboard/",
 });
